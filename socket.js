9d60588acc4e34c8df675cdf9afe0eef342fbfed
= <=>
2021-09-14 22:15:22 +0300

    Create a fake topmenu to help development of the wiki

--- socket.js
+++ socket.js
@@ -1,6 +1,21 @@
 /**
- * Very "complex" reload system
+ * To block scope everything we do here
  */
-const ws = new WebSocket("ws://localhost:5050")
+(() => {
+     /**
+   * Very "complex" reload system
+   */
+     const ws = new WebSocket("ws://localhost:5050")
 
-ws.onmessage = () => location.reload()
+     ws.onmessage = () => location.reload()
+
+     /**
+      * Inject a fake iGEM Top Menu
+      */
+     const topmenu = document.createElement("div")
+
+     topmenu.setAttribute("style", "position: fixed; width: 100%; top: 0px; left: 0px; height: 16px; background-color: #383838; border-bottom: 2px solid black; z-index: 9999;")
+
+     document.body.prepend(topmenu)
+
+})();
